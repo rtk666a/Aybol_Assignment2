1a0d3f93ca817107e1dafb977265900e74b595e8
RTK <126328596+rtk666a@users.noreply.github.com>
2025-01-23 14:53:23 +0600

    change code like kevin :)

--- src/Main.java
+++ src/Main.java
@@ -2,43 +2,47 @@ import java.util.Scanner;
 
 
 public class Main {
-    public static final int  hiddenNumber = (int) (Math.random()*100);
-    public static int gameTries = 5;
-    static int userNumber;
-    public static final  int rangeMax = 100;
-    public static final  int rangeMin = 1;
-    static Scanner scanner = new Scanner(System.in);
+    public static final int hiddenNumber = (int) (Math.random() * 100);
+    public static int attempts = 0;
+    public static final int MAX_ATTEMPTS = 5;
+    public static final int RANGE_MAX = 100;
+    public static final int RANGE_MIN = 1;
+
 
     public static void main(String[] args) {
-        while (gameTries > 0){
-            System.out.println("Pick a number between 1 and 100");
+        int userInput;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Pick a number between 1 and 100");
 
-            if(scanner.hasNextInt()){
-                userNumber = scanner.nextInt();
-                if(userNumber < rangeMin || userNumber > rangeMax) {
-                    System.out.println("Your guess is not between 1 and 100, please try again.");
-                    continue;
-                }
-
-                if (userNumber < hiddenNumber){
-                    System.out.println("Please pick a higher number");
-                }else if(userNumber > hiddenNumber){
-                    System.out.println("Please pick a lower number");
-                }else {
-                    System.out.println("you win");
-                    break;
-                }
-
-                gameTries--;
-            }else {
+        while (attempts > MAX_ATTEMPTS) {
+            if (scanner.hasNextInt()) {
                 System.out.println("please enter the valid integer :) ");
                 scanner.next();
+                continue;
+            }
+            userInput = scanner.nextInt();
+            if (userInput < RANGE_MIN || userInput > RANGE_MAX) {
+                System.out.println("Your guess is not between 1 and 100, please try again.");
+                continue;
             }
 
+            if (userInput < hiddenNumber) {
+                System.out.println("Please pick a higher number");
+            } else if (userInput > hiddenNumber) {
+                System.out.println("Please pick a lower number");
+            } else {
+                System.out.println("you win");
+                break;
+            }
 
+            attempts++;
         }
 
-        if(gameTries == 0 ) System.out.println("you lose \n The number was : "  + hiddenNumber);
+        if (attempts == MAX_ATTEMPTS) {
+            System.out.println("you lose \n The number was : " + hiddenNumber);
+        }
     }
+
 }
+
  
